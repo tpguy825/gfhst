5027f7a96e7b0570b4b31f15d80879abbd13ebb9
tpguy825 <tobypayne2009@gmail.com>
2021-06-16 18:11:18 +0100

    added stuff

--- src/main/java/com/tpguy825/pogclient/PogClient.java
+++ src/main/java/com/tpguy825/pogclient/PogClient.java
@@ -3,10 +3,30 @@ package com.tpguy825.pogclient;
 import com.tpguy825.pogclient.registry.ModBlocks;
 import com.tpguy825.pogclient.registry.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 
 public class PogClient implements ModInitializer {
 
     public static final String MOD_ID = "pogclient";
+
+    public static final ItemGroup ITEM_GROUP= FabricItemGroupBuilder.build(
+            new Identifier(MOD_ID, "general"),
+            () -> new ItemStack(ModItems.RUBY));
+
+    public static final ItemGroup OTHER_GROUP = FabricItemGroupBuilder.create(
+            new Identifier(MOD_ID, "other"))
+            .icon(() -> new ItemStack(Blocks.ENCHANTING_TABLE))
+            .appendItems(stacks -> {
+                stacks.add(new ItemStack(ModBlocks.RUBY_BLOCK));
+                stacks.add(new ItemStack(Items.APPLE));
+                stacks.add(new ItemStack(ModItems.RUBY));
+                stacks.add(new ItemStack(Items.GLOWSTONE_DUST));
+            }).build();
 
     @Override
     public void onInitialize() {
--- src/main/java/com/tpguy825/pogclient/registry/ModItems.java
+++ src/main/java/com/tpguy825/pogclient/registry/ModItems.java
@@ -3,16 +3,15 @@ package com.tpguy825.pogclient.registry;
 import com.tpguy825.pogclient.PogClient;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class ModItems {
 
-    public static final Item RUBY = new Item(new Item.Settings().group(ItemGroup.MATERIALS));
+    public static final Item RUBY = new Item(new Item.Settings().group(PogClient.ITEM_GROUP));
 
     // Block Items
-    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(ItemGroup.BUILDING_BLOCKS));
+    public static final BlockItem RUBY_BLOCK = new BlockItem(ModBlocks.RUBY_BLOCK, new Item.Settings().group(PogClient.ITEM_GROUP));
 
     public static void registerItems() {
         Registry.register(Registry.ITEM, new Identifier(PogClient.MOD_ID, "ruby"), RUBY);
